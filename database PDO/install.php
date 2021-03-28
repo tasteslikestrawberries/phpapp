c95151aea4abc66a106870361f9c2cc2d6964849
tasteslikestrawberries <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-03-28 17:00:53 +0200

    up

--- database PDO/install.php
+++ database PDO/install.php
@@ -6,33 +6,14 @@
   *
   */
 
-  if (isset($_POST['submit'])) {
-    require "../config.php";
-  
-    try {
-        $connection = new PDO($dsn, $username, $password, $options);
+require "config.php";
 
-        $new_user = array(
-          "firstname" => $_POST['firstname'],
-          "lastname"  => $_POST['lastname'],
-          "email"     => $_POST['email'],
-          "age"       => $_POST['age'],
-          "location"  => $_POST['location']
-        );
-        
-        $sql = sprintf(
-            "INSERT INTO %s (%s) values (%s)",
-            "users",
-            implode(", ", array_keys($new_user)),
-            ":" . implode(", :", array_keys($new_user))
-        );
-        
-        $statement = $connection->prepare($sql);
-        $statement->execute($new_user);
-  
-    } catch(PDOException $error) {
-      echo $sql . "<br>" . $error->getMessage();
-    }
-  
-  }
+try {
+  $connection = new PDO("mysql:host=$host", $username, $password, $options);
+  $sql = file_get_contents("data/init.sql");
+  $connection->exec($sql);
 
+  echo "Database and table users created successfully.";
+} catch(PDOException $error) {
+  echo $sql . "<br>" . $error->getMessage();
+}
